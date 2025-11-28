3d946b14cc879224ae0caeec47dce34c57b5b9c7
chetan jangid <jangidchetan.jpr@gmail.com>
2025-11-28 20:48:07 +0530

    Update underfitting_overfitting_goodfit.py

--- underfitting_overfitting_goodfit.py
+++ underfitting_overfitting_goodfit.py
@@ -1,1 +1,33 @@
-  
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+# Create sample data (non-linear curve)
+np.random.seed(0)
+X = np.linspace(0, 10, 20)
+y = np.sin(X) + np.random.normal(0, 0.2, len(X))  # curved data
+
+X = X.reshape(-1, 1)
+
+# 1. Underfitting: Linear Regression (degree 1)
+model_under = LinearRegression()
+model_under.fit(X, y)
+
+# 2. Good Fit: Polynomial Degree 3
+model_good =make_pipeline(PolynomialFeatures(3), LinearRegression())
+model_good.fit(X, y)
+
+# 3. Overfitting: Polynomial Degree 10
+model_over =make_pipeline(PolynomialFeatures(10), LinearRegression())
+model_over.fit(X, y)
+
+# Prediction range
+X_test = np.linspace(0, 10, 200).reshape(-1, 1)
+
+y_under = model_under.predict(X_test)
+y_good  = model_good.predict(X_test)
+y_over  = model_over.predict(X_test)
+
+
