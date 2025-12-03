a7dc763cff6f1d181deb71fac61fbfff30f720af
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-03 21:32:25 +0530

    Update elastic_net.py
    importing liibraries

--- elastic_net.py
+++ elastic_net.py
@@ -1,1 +1,6 @@
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import ElasticNet
+df=pd.read_csv("elasticnet.csv")
+model_elasticnet=ElasticNet(alpha=1,l1_ratio=0.5)
