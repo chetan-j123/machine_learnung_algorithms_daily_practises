43e514a8a6047f7a2d189be77067158626498714
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-04 19:09:13 +0530

    Update elastic_net.py

--- elastic_net.py
+++ elastic_net.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet,LassoCV
 df=pd.read_csv("elasticnet.csv")
 model_elasticnet=ElasticNet(alpha=1,l1_ratio=0.5)
 x=df.drop(columns=["target"])
@@ -20,4 +20,18 @@ for i in range(0,len(weights),1):
       remaining_features.append(weights[i])
 print("remianied features total = ",len(remaining_features))
 print("removed features = ",len(features_which_are_removed))
-
+model_lasso=LassoCV(alphas=[0.5,1])
+model_lasso.fit(x,y)
+weights=np.array(model_lasso.coef_)
+features=np.array(df.columns)
+features_which_are_removed=[]
+remaining_features=[]
+#print("feture ",features[1],"with weight",weights[1])
+for i in range(0,len(weights),1):
+    if weights[i]==0:
+      features_which_are_removed.append(weights[i])
+    else:
+      remaining_features.append(weights[i])
+print("remianied features total = ",len(remaining_features))
+print("removed features = ",len(features_which_are_removed))
+#here difference are clear lasso model removed many features 85% features are removed and we knnow the many features  are correlated so it ignores this satstement so wehy here we chooses elastic net bcz it removes only 65% features and  it maintains the respect of correlation statemnet 
