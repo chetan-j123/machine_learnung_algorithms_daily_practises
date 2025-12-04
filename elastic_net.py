6035fcd80ccebe8fa50c3cd1153445cac6ef1f70
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-04 09:01:16 +0530

    Update elastic_net.py

--- elastic_net.py
+++ elastic_net.py
@@ -4,3 +4,20 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import ElasticNet
 df=pd.read_csv("elasticnet.csv")
 model_elasticnet=ElasticNet(alpha=1,l1_ratio=0.5)
+x=df.drop(columns=["target"])
+x=x.values
+y=df["target"].values
+model_elasticnet.fit(x,y)
+weights=np.array(model_elasticnet.coef_)
+features=np.array(df.columns)
+features_which_are_removed=[]
+remaining_features=[]
+#print("feture ",features[1],"with weight",weights[1])
+for i in range(0,len(weights),1):
+    if weights[i]==0:
+      features_which_are_removed.append(weights[i])
+    else:
+      remaining_features.append(weights[i])
+print("remianied features total = ",len(remaining_features))
+print("removed features = ",len(features_which_are_removed))
+
