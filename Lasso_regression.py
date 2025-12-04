501a3aebd142d598f7cf4e499d47ced7984c35b9
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-04 21:36:00 +0530

    Update Lasso_regression.py

--- Lasso_regression.py
+++ Lasso_regression.py
@@ -16,48 +16,43 @@ x=sc.fit_transform(x)
 model.fit(x,y)
 d=np.array(model.coef_)
 print(len(d))
-
 removed=[]
 remains=[]
 ke=np.array(df2_for_input.columns)
 #ke=np.array(df2_for_input.keys)
 for i in range(0,len(d),1):
-    if d[i]==0:
+    if abs(d[i])<1e-6:
         removed.append(ke[i])
     else:
         remains.append(ke[i])
 print("removed features are ",removed)
 print("remained features are ",len(remains),"which is as following =",remains)
 #here we did features selection by using lasso regressio lasso removes that h=deatues that are not affecting the trget or and whichs weights becomme 0 while pemnalty givens to weights
-
 #sparse model requiremnet
 df2=pd.read_csv("data2.csv")
 x1=df2.drop(columns=["target"])
 x2=x1.values
-y1=df2["target"].values
-
-model2 = LassoCV(alphas=np.logspace(-4, 3, 500))
-
 x2=sc.fit_transform(x2)
+y1=df2["target"].values
+model2 = LassoCV(alphas=np.logspace(-4, 3, 500))
 model2.fit(x2,y1)
 features=np.array(x1.columns)
 removed1=[]
 remains1=[]
-
-
 weights=np.array(model2.coef_)
 for i in range(0,len(features),1):
-    if weights[i]==0:
+    if abs(weights[i])<1e-6:
         removed1.append(features[i])
     else:
         remains1.append(features[i])
 print("total valuable fratures are = ",len(remains1))
 print("useless are ",len(removed1))
 #for moblie sparse requiremnet we can remove only 16 features it comes after high strength of penalty so remained 14 features are highly valuables
-#beacuse the 14 are highly correlated so train a ridge model baseed on 14 features 
+#beacuse the 14 are highly correlated so train a ridge model baseed on 14
 new_x=x1.drop(columns=removed1)
 print(new_x.ndim,new_x.shape,len(new_x))
-new_model=RidgeCV(alphas=[np.random.randint(0.1,10)])
+new_model=RidgeCV(alphas=[0.1, 1, 10, 100, 1000])
+new_x=sc.fit_transform(new_x)
 new_model.fit(new_x,y1)
 feature=[]
 for i in range(0,14,1):
@@ -65,6 +60,5 @@ for i in range(0,14,1):
   feature.append(fea)
 feature=np.array(feature)
 feature=feature.reshape(1,-1)
+feature=sc.transform(feature)
 print("target is = ",new_model.predict(feature)[0])
-
-
