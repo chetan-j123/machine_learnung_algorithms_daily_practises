9e4a2d0544bc9469959d9bc8d390f9d1f60feb38
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-06 22:03:34 +0530

    Update decision_tree_classification.py
    why to choose decision tree classification instead of logistic regression

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -1,4 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+#why we use decision tree clsssififctaion instead of logitic 
+"""
+logistic regression have linear boundries 
+if a straight line cant divide the data in 2 equal parts then logistic fails
+where DT have complex non linear bounddries
+means row complex is better approachable with dt classifictaion
+highly coreelated features k liye bhi work krta 
+outliers se effect nhi hota
+no  need to change categorical data into numerical 0 or 1
+"""
