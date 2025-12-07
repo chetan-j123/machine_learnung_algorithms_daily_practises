98584cdec78d90ae5f3958bda32e862601bd5900
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-07 21:56:03 +0530

    Update decision_tree_classification.py

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -11,6 +11,7 @@ no  need to change categorical data into numerical 0 or 1
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 data=[
     {'Hours_Studied': 2, 'Attendance': 60, 'Pass': 'No'},
@@ -25,6 +26,8 @@ x=data[["Hours_Studied","Attendance"]].values
 y=data["Pass"].values
 model=DecisionTreeClassifier()
 model.fit(x,y)
+tree.plot_tree(model,filled=True,feature_names=["Hours_Studied","Attendance"],class_names=["No","Yes"])#phle wala 0 treat hota agla 1 jese
+plt.show()
 study_hrs=int(input("enter your study hrs="))
 attendancew=int(input("enter your attendance"))
 arr=[]
@@ -32,4 +35,5 @@ arr.append(study_hrs)
 arr.append(attendancew)
 arr=np.array(arr)
 arr=arr.reshape(1,-1)
+#tree ploting
 print("you will get =  ",model.predict(arr)[0])
