cdd0da8deeebb4128d052acb904fb00c16eb7198
chetan jangid <jangidchetan.jpr@gmail.com>
2025-12-10 09:47:26 +0530

    Update decision_tree_regression.py

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -14,7 +14,7 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42
 simple_overfitted_model=DecisionTreeRegressor(random_state=42,criterion="squared_error")
 simple_overfitted_model.fit(x_train,y_train)
 y_pred_with_overfit_model=simple_overfitted_model.predict(x_test)
-test_accuaracy_of_overfitmodel=r2_score(y_pred_with_overfit_model,y_test)
+test_accuaracy_of_overfitmodel=r2_score(y_test,y_pred_with_overfit_model)
 p=simple_overfitted_model.predict(x_train)
 trainnning_accuracy_of_overfitmodel=r2_score(p,y_train)
 print(test_accuaracy_of_overfitmodel)#very low to low
@@ -31,7 +31,7 @@ params={
 grid=GridSearchCV(
     DecisionTreeRegressor(criterion="squared_error",random_state=42),
     params,
-    cv=7
+    cv=5
 )
 grid.fit(x_train,y_train)
 print(grid.best_params_)
@@ -45,8 +45,9 @@ min_samples_split=dictionary["min_samples_split"]
 )
 pre_prunned_decsion_tree_regression_model.fit(x_train,y_train)
 y_pred_wiht_pre_prunned_model=pre_prunned_decsion_tree_regression_model.predict(x_test)
-test_accuracy_of_pre_prunned_model=r2_score(y_pred_wiht_pre_prunned_model,y_test)
+test_accuracy_of_pre_prunned_model=r2_score(y_test,y_pred_wiht_pre_prunned_model)
 print("test accuracy increases ",(test_accuracy_of_pre_prunned_model-test_accuaracy_of_overfitmodel)*100,"%","after using pre prunned model")
+
 #for see  the accuracy score lets choose post prunning
 path=simple_overfitted_model.cost_complexity_pruning_path(x_train,y_train)
 best_alphas=path.ccp_alphas
@@ -57,7 +58,7 @@ for i in range(0,len(best_alphas),1):
     temporary_model=DecisionTreeRegressor(random_state=42,criterion="squared_error",ccp_alpha=best_alphas[i])
     temporary_model.fit(x_train,y_train)
     y_pred=temporary_model.predict(x_test)
-    r_Square_score=r2_score(y_pred,y_test)
+    r_Square_score=r2_score(y_test,y_pred)
     r_sq_scores.append(r_Square_score)
     alphas.append(best_alphas[i])
 r_sq_scores=np.array(r_sq_scores)
@@ -70,6 +71,6 @@ print("best alphas is = ",best_alpha_,"with r2 score = ",highest_r2_score_is)
 post_prunned_decsion_tree_regression_model=DecisionTreeRegressor(random_state=42,criterion="squared_error",ccp_alpha=best_alpha_)
 post_prunned_decsion_tree_regression_model.fit(x_train,y_train)
 y_pred_wiht_post_prunned_model=post_prunned_decsion_tree_regression_model.predict(x_test)
-test_accuracy_of_post_prunned_model=r2_score(y_pred_wiht_post_prunned_model,y_test)
+test_accuracy_of_post_prunned_model=r2_score(y_test,y_pred_wiht_post_prunned_model)
 print("test accuracy increases ",(test_accuracy_of_post_prunned_model-test_accuaracy_of_overfitmodel)*100,"%","with post prunned model")
-#here psot prunned model shows more accuracy bcz of the htype of dataset the dataset contains only 350 row  which is samall
+#here psot prunned model shows more accuracy bcz of the dtype of dataset the dataset contains only 350 row  which is samall
